75dd9ef769fa54dff65432be09c09f9ccbf6d6d1
Stanislas Bernard <bernardstanislas@gmail.com>
2020-01-31 14:52:47 +0100

    :alembic: Add WIP admin

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(morgan('combined'))
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
